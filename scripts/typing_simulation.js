b5185fff0a865020cc364969c2aec4d9ffc28aa1
kboro <kkborowy@gmail.com>
2024-04-07 13:03:59 +0200

    typing simulation animation

---
+++ scripts/typing_simulation.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let text = 'hello, world!'
+    let speed = 170
+
+    let index = 0
+    const typing = setInterval(() => {
+        document.getElementById('simulateTyping').textContent += text[index];
+        index++;
+        if (index == text.length) {
+            clearInterval(typing)
+        }
+    }, speed)
+})
